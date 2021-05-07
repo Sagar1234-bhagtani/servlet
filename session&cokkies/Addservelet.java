b45c6e4dfd4703988acf601a16984cef27655696
Sagar1234-bhagtani <65902030+Sagar1234-bhagtani@users.noreply.github.com>
2021-05-07 20:26:02 +0530

    Add files via upload

---
+++ session&cokkies/Addservelet.java
@@ -0,0 +1,42 @@
+package com.sagar;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Addservelet extends HttpServlet {
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
+	{
+		int i=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
+		int k=i+j;
+		//session
+		//HttpSession session=req.getSession();
+		//session.setAttribute("k",k);
+		//session end
+		
+		
+		//cookie 
+		Cookie cookie=new Cookie("k",k+"");
+		res.addCookie(cookie);
+		
+		//cookie end
+		
+		
+		res.sendRedirect("sq?k="+k); 
+		//req.setAttribute("k",k);
+		//RequestDispatcher rd=req.getRequestDispatcher("sq");
+	//	rd.forward(req,res);
+		//PrintWriter out=res.getWriter();
+		//out.println("result is"+k);
+	}
+	
+
+}
---
+++ session&cokkies/sqservlet.java
@@ -0,0 +1,41 @@
+package com.sagar;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class sqservlet extends HttpServlet {
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		//int k=(int)req.getAttribute("k");
+		//int k=Integer.parseInt(req.getParameter("k"));
+		
+		//Session
+		//HttpSession session=req.getSession();
+		//session.removeAttribute("k");//for removing session this line
+		//int k=(int)session.getAttribute("k");
+		
+		//Session end
+		//cookie
+		int k=0;
+		Cookie cookies[]=req.getCookies();
+		
+		for(Cookie c: cookies)
+		{
+			if(c.getName().equals("k"))
+				k=Integer.parseInt(c.getValue());
+		}
+		
+		//cookie end
+		
+		k=k*k;
+		PrintWriter out=res.getWriter();
+		out.println("result is"+k);
+	}
+
+}
