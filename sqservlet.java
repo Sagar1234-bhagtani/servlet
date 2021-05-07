83923f7342083790478141f044a687e34097f43a
Sagar1234-bhagtani <65902030+Sagar1234-bhagtani@users.noreply.github.com>
2021-05-07 10:38:06 +0530

    Add files via upload

---
+++ sqservlet.java
@@ -0,0 +1,19 @@
+package com.sagar;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class sqservlet extends HttpServlet {
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		int k=(int)req.getAttribute("k");
+		k=k*k;
+		PrintWriter out=res.getWriter();
+		out.println("result is"+k);
+	}
+
+}
