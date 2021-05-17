3e30beaf975633ecb5f368c2709a582d485fa28e
Sagar1234-bhagtani <65902030+Sagar1234-bhagtani@users.noreply.github.com>
2021-05-17 16:48:06 +0530

    Add files via upload

---
+++ login project/logout back secure/Login.java
@@ -0,0 +1,37 @@
+package com.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//whenever sending data from client use post.
+		//whenever fetching data from use Get. 
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	String uname=request.getParameter("uname");
+	String pass=request.getParameter("pass");
+	
+	if(uname.equals("sagar")&&pass.equals("learning"))
+	{
+	 HttpSession session=request.getSession();
+	 session.setAttribute("username", uname);
+	 response.sendRedirect("welcome.jsp");
+
+	
+	}
+	else
+	{
+		response.sendRedirect("login.jsp");
+	}
+
+}
+}
---
+++ login project/logout back secure/aboutus.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+THIS IS SAGAR BHAGTANI.I'M THE STUDENTIN AITH AND HAVING LOT OF INTEREST IN CODING.
+
+</body>
+</html>
