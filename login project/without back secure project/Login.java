1e1c31bb0253f9c77f8432417494151281c7e526
Sagar1234-bhagtani <65902030+Sagar1234-bhagtani@users.noreply.github.com>
2021-05-17 15:50:09 +0530

    Add files via upload

---
+++ login project/without back secure project/Login.java
@@ -0,0 +1,35 @@
+package com.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	String uname=request.getParameter("uname");
+	String pass=request.getParameter("pass");
+	
+	if(uname.equals("sagar")&&pass.equals("learning"))
+	{
+	 HttpSession session=request.getSession();
+	 session.setAttribute("username", uname);
+	 response.sendRedirect("welcome.jsp");
+
+	
+	}
+	else
+	{
+		response.sendRedirect("login.jsp");
+	}
+
+}
+}
---
+++ login project/without back secure project/videos.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<% 
+if(session.getAttribute("username")==null)
+{
+	response.sendRedirect("login.jsp");
+}
+	
+	
+	
+	
+	
+	%>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/-FkYxCW0sEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<form action="Logout">
+	<input type="submit" value="Logout">
+	</form>
+</body>
+</html>
